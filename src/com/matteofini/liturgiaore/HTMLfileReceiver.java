c896702e418c772193a8b6b89de8989109401313
matteofini <mf.calimero@gmail.com>
2011-11-07 09:50:28 +0100

    versionCode=9 versionName=2.0

--- src/com/matteofini/liturgiaore/HTMLfileReceiver.java
+++ src/com/matteofini/liturgiaore/HTMLfileReceiver.java
@@ -1,3 +1,21 @@
+/**
+ *  Program: Liturgia delle Ore
+ *  Author: Matteo Fini <mf.calimero@gmail.com>
+ *  Year: 2011
+ *  
+ *	This file is part of "Liturgia delle Ore".
+ *	"Liturgia delle Ore" is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  "Liturgia delle Ore" is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 package com.matteofini.liturgiaore;
 
 import android.content.BroadcastReceiver;
